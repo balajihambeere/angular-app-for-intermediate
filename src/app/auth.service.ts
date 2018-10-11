1223ca2747d3d319271ba962351855cb765b6580
balaji hambeere <balaji.hambeere@hotmail.com>
2018-10-11 19:08:49 +0530

    working on auth model

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from './user.model';
+import { User } from './auth.model';
 import { Observable, BehaviorSubject, ReplaySubject } from 'rxjs';
 import { catchError, map, tap, distinctUntilChanged } from 'rxjs/operators';
 import { JwtService } from './jwt.service';
 
 @Injectable()
-export class UserService {
+export class AuthService {
     private currentUserSubject = new BehaviorSubject<User>(new User());
     public currentUser = this.currentUserSubject.asObservable().pipe(distinctUntilChanged());
 
@@ -32,9 +32,9 @@ export class UserService {
         }
     }
 
-    authentication(type, source): Observable<User> {
+    auth(type: String, user: User): Observable<User> {
         const route = (type === 'login') ? 'api/account/login' : 'api/account/register';
-        return this.http.post<User>(route, source)
+        return this.http.post<User>(route, user)
             .pipe(map(data => {
                 if (type === 'login') {
                     this.setAuth(data);
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+    selector: 'app-auth',
+    templateUrl: 'auth.component.html'
+})
+export class AuthComponent implements OnInit {
+    authForm: FormGroup;
+    authType: String = '';
+    title: String = '';
+    isSubmitting = false;
+    buttonTitle: String = '';
+
+    constructor(private route: ActivatedRoute,
+        private router: Router,
+        private fb: FormBuilder,
+        private authService: AuthService) {
+        this.authForm = this.fb.group({
+            'userName': ['', Validators.required],
+            'password': ['', Validators.required]
+        });
+    }
+
+    ngOnInit() {
+        this.route.url.subscribe(data => {
+            this.authType = data[data.length - 1].path;
+            this.title = (this.authType === 'login') ? 'Please login here..' : 'Create a new account';
+            this.buttonTitle = (this.authType === 'login') ? 'Login' : 'Create Account';
+            if (this.authType === 'register') {
+                this.authForm.addControl('confirmPassword', new FormControl('', Validators.required));
+            }
+        });
+    }
+
+
+
+    submitForm() {
+        this.isSubmitting = true;
+        const credentials = this.authForm.value;
+        this.authService.auth(this.authType, credentials).subscribe(data => {
+            if (this.authType === 'register') {
+                this.router.navigateByUrl('login');
+            } else {
+                this.router.navigateByUrl('/');
+            }
+        });
+    }
+}
