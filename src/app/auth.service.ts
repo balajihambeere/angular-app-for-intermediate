f54daca802e022cedb56089dccd86cf545a8ccb7
balaji hambeere <balaji.hambeere@hotmail.com>
2018-10-18 19:13:13 +0530

    fixed some issues

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { User } from './auth.model';
 import { Observable, BehaviorSubject, ReplaySubject } from 'rxjs';
-import { catchError, map, tap, distinctUntilChanged } from 'rxjs/operators';
+import { map, distinctUntilChanged } from 'rxjs/operators';
 import { JwtService } from './jwt.service';
 import { Router } from '@angular/router';
 import { environment } from '../environments/environment';
